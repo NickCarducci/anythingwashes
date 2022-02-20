407073fd3999d4d4e953f7d7b10eeba2ccd2a04a
Nick <nmcarducci@gmail.com>
2022-02-20 14:49:23 -0500

    ok

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        fontFamily: "'Audiowide', cursive"
+      }}
+    >
+      <h1>Teachers and experts like activists</h1>
+      <h2>
+        investment bank, amortized-balance(
+        <a href="https://www.yardeni.com/pub/monetagg.pdf">$170t</a>
+        good-will:$167tliabilities){" "}
+        <span role="img" aria-label="sun">
+          ☀️
+        </span>
+      </h2>
+    </div>
+  );
+}
